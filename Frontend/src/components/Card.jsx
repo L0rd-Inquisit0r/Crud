1b05b0d4707f672a58ecf430639d037a4b840450
L0rd-Inquisit0r <sean.s.l.duran@gmail.com>
2024-10-02 12:26:30 +0800

    created card component

---
+++ Frontend/src/components/Card.jsx
@@ -0,0 +1,12 @@
+function Card()
+{
+    return(
+        <div className="card">
+            <img></img>
+            <h2>title</h2>
+            <p>desc.</p>
+        </div>                    
+    );
+}
+
+export default Card
