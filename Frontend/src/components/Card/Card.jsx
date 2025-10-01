3ef47dc6ecc60677ac203f324a8f49c1461859ec
Siegescript_ <sean.s.l.duran@gmail.com>
2025-10-01 15:17:09 +0800

    added image to card component

--- Frontend/src/components/Card/Card.jsx
+++ Frontend/src/components/Card/Card.jsx
@@ -1,10 +1,11 @@
 import styles from './Card.module.css'
+import image from '../../assets/react.svg'
 
 function Card()
 {
     return(
         <div className={styles.card}>
-            <img src='' alt='profile picture'></img>
+            <img src={image} alt='profile picture'/>
             <h2>Title</h2>
             <p>Lorem ipsum dolor sit amet</p>
         </div>                    
