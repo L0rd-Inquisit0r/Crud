2c31d03a56667213573cd936bba3b5014f0e5a19
Siegescript_ <sean.s.l.duran@gmail.com>
2025-10-01 15:17:28 +0800

    added image next to title

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
+import icon from '../../assets/react.svg'
 
 function Header(){
 
     return(
         <header>
-            <h1>CRUD</h1>
+            <h1>CRUD<img src={icon} alt='icon'/></h1>
         </header>
     );
 }
