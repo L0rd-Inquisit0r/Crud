9af28002c3c8e73d5d7094d31cb3430fcae2225f
Siegescript_ <sean.s.l.duran@gmail.com>
2025-10-04 22:10:01 +0800

    edit: renaming sections directory to layout

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import Header from "./sections/Header"
-import Footer from "./sections/Footer"
-import Main from "./sections/Main";
-import Nav from "./sections/Nav";
-import Sidebar from "./sections/Side";
+import Header from "./layout/Header"
+import Footer from "./layout/Footer"
+import Main from "./layout/Main";
+import Nav from "./layout/Nav";
+import Sidebar from "./layout/Side";
 
 function App() {
 
